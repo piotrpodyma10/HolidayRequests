7a3f19cf3ba1f23fc1e0e2e07ba9520f12402962
Piter <piotr.podyma10@gmail.com>
2019-05-09 13:45:04 +0200

    Send an email after saving leave request

--- Backend/HolidayRequests/Controllers/LeaveRequestsController.cs
+++ Backend/HolidayRequests/Controllers/LeaveRequestsController.cs
@@ -8,16 +8,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace HolidayRequests.Controllers
 {
     public class LeaveRequestsController : Controller
     {
         private readonly HolidayRequestsContext _context;
+        private IHostingEnvironment _env;
 
-        public LeaveRequestsController(HolidayRequestsContext context)
+        public LeaveRequestsController(HolidayRequestsContext context, IHostingEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         [HttpGet("GetLeaveRequestByUser")]
@@ -110,12 +116,63 @@ namespace HolidayRequests.Controllers
                 allDaysOff.ActualLeaveDaysNumber = allDaysOff.ActualLeaveDaysNumber - request.DaysOff;
                 _context.SaveChanges();
 
+
+                var employee = _context.Employees.Where(x => x.Id == request.EmployeeId).FirstOrDefault();
+                var employeeEmailData = new EmployeeEmailRequest
+                {
+                    Name = employee.FirstName + " " + employee.LastName,
+                    StartDate = request.StartDate.ToString().Substring(0, 9),
+                    EndDate = request.EndDate != null ? request.EndDate.ToString().Substring(0, 9) : "-",
+                    DaysOff = request.DaysOff.ToString()
+                };
+
+                var fromAddress = new MailAddress("snowy2night@gmail.com", "Holiday request app");
+                var toAddress = new MailAddress("gooddayis2day@gmail.com", "To Name");
+                const string fromPassword = "snow!123";
+                const string subject = "Leave request information";
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = CreateBody(employeeEmailData),
+                    IsBodyHtml = true
+                })
+                {
+                    smtp.Send(message);
+                }
+
                 return Ok();
             }
             catch (Exception)
             {
                 throw;
             }
+        }
+
+        private string CreateBody(EmployeeEmailRequest employee)
+        {
+            string body = string.Empty;
+            var webRoot = _env.WebRootPath.Substring(0, _env.WebRootPath.Length-8);
+            var file = System.IO.Path.Combine(webRoot, "EmailTemplates/SendRequest.html");
+            using (StreamReader reader = new StreamReader(file))
+            {
+                body = reader.ReadToEnd();
+            }
+
+            body = body.Replace("{employeeName}", employee.Name);
+            body = body.Replace("{startDate}", employee.StartDate);
+            body = body.Replace("{endDate}", employee.EndDate);
+            body = body.Replace("{daysOff}", employee.DaysOff);
+
+            return body;
         }
 
         [HttpPut("EditLeaveRequest")]
